3ed917d2331fcd650f867445d615d3d8accd0835
umt0120 <uemoto0120@gmail.com>
2022-09-24 12:11:22 +0900

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+This app has been Created With Electron Forge.
+
+See link below.
+
+- https://www.electronforge.io/templates/typescript-+-webpack-template
+- https://www.electronforge.io/guides/framework-integration/react-with-typescript
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+function render() {
+  const root = createRoot(document.getElementById('app'));
+  root.render(<App />);
+}
+
+
+function App () {
+  return (<h2>Hello from React!</h2>)
+}
+
+render()
