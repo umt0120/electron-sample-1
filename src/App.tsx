1e1632b621cb57a5831ea4fb1e7a0f4a3e18aa10
umt0120 <uemoto0120@gmail.com>
2022-09-25 22:59:04 +0900

    test: add test setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 
 function render() {
-  const root = createRoot(document.getElementById('app'));
-  root.render(<App />);
+	const root = createRoot(document.getElementById('app'));
+	root.render(<App/>);
 }
 
 
-function App () {
-  return (<h2>Hello from React!</h2>)
+function App() {
+	return (
+		<>
+			<h2>Hello from React!</h2>
+		</>
+	)
 }
 
 render()
---
+++ test/Hello.spec.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+// import {render} from '@testing-library/react'
+import {render, unmountComponentAtNode} from "react-dom";
+import {act} from "react-dom/test-utils";
+
+import Hello from "../src/Hello";
+
+let container: HTMLDivElement = null;
+beforeEach(() => {
+	container = document.createElement("div")
+	document.body.appendChild(container)
+})
+
+afterEach(() => {
+	unmountComponentAtNode(container)
+	container.remove()
+	container = null;
+})
+
+it('renders with or without a name', () => {
+	act(() => {
+		render(<Hello name={null}/>, container)
+	})
+	expect(container.textContent).toBe("Hey, stranger!")
+	act(() => {
+		render(<Hello name="Jenny"/>, container)
+	})
+	expect(container.textContent).toBe("Hello, Jenny!")
+	act(() => {
+		render(<Hello name="Margaret"/>, container)
+	})
+	expect(container.textContent).toBe("Hello, Margaret!")
+})
