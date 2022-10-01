cca2fb9834e92c35fd9c91ffcd05a49475c2ba49
umt0120 <uemoto0120@gmail.com>
2022-10-01 22:22:32 +0900

    test: add snapshot test

--- test/components/Hello.spec.tsx
+++ test/components/Hello.spec.tsx
@@ -1,33 +1,37 @@
 import React from "react";
 // import {render} from '@testing-library/react'
-import {render, unmountComponentAtNode} from "react-dom";
-import {act} from "react-dom/test-utils";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import pretty from "pretty";
 
 import Hello from "../../src/components/Hello";
 
 let container: HTMLDivElement = null;
 beforeEach(() => {
-	container = document.createElement("div")
-	document.body.appendChild(container)
-})
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
 
 afterEach(() => {
-	unmountComponentAtNode(container)
-	container.remove()
-	container = null;
-})
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
 
-it('renders with or without a name', () => {
-	act(() => {
-		render(<Hello name={null}/>, container)
-	})
-	expect(container.textContent).toBe("Hey, stranger!")
-	act(() => {
-		render(<Hello name="Jenny"/>, container)
-	})
-	expect(container.textContent).toBe("Hello, Jenny!")
-	act(() => {
-		render(<Hello name="Margaret"/>, container)
-	})
-	expect(container.textContent).toBe("Hello, Margaret!")
-})
+it("renders with or without a name", () => {
+  act(() => {
+    render(<Hello name={null} />, container);
+  });
+  expect(pretty(container.innerHTML)).toMatchInlineSnapshot(
+    `"<span>Hey, stranger!</span>"`
+  );
+  expect(container.textContent).toBe("Hey, stranger!");
+  act(() => {
+    render(<Hello name="Jenny" />, container);
+  });
+  expect(container.textContent).toBe("Hello, Jenny!");
+  act(() => {
+    render(<Hello name="Margaret" />, container);
+  });
+  expect(container.textContent).toBe("Hello, Margaret!");
+});
